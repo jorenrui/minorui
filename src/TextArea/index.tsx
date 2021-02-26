0bf0070e4f85ac6579405ae9c2a806e62bf07d8c
Joeylene Rivera <joeylenerivera@gmail.com>
2021-02-27 02:21:54 +0800

    feat: add variant and color to text area

--- src/TextArea/index.tsx
+++ src/TextArea/index.tsx
@@ -7,15 +7,56 @@ export const TextArea = styled('textarea', {
   display: 'block',
   width: '100%',
   border: 'none',
-  boxShadow: 'inset 0 0 0 1px $colors$primary300',
-  padding: '$40 $50',
-  borderRadius: '$md',
+  borderRadius: '$sm',
   outlineColor: 'currentColor',
-  color: '$text',
-  fontType: 'base',
   resize: 'vertical',
 
   '&:hover, &:focus, &:active': {
     boxShadow: 'inset 0 0 0 1px $colors$primary',
-  }
+  },
+
+  variants: {
+    variant: {
+      info: {
+        boxShadow: 'inset 0 0 0 1px $colors$blue600',
+      },
+      warning: {
+        boxShadow: 'inset 0 0 0 1px $colors$yellow600',
+      },
+      error: {
+        boxShadow: 'inset 0 0 0 1px $colors$red600',
+      },
+      success: {
+        boxShadow: 'inset 0 0 0 1px $colors$green600',
+      },
+      accent: {
+        boxShadow: 'inset 0 0 0 1px $colors$accent600',
+      },
+      default: {
+        boxShadow: 'inset 0 0 0 1px $colors$primary300',
+      },
+    },
+    size: {
+      xs: {
+        padding: '$30 $40',
+        fontType: 'xs',
+      },
+      sm: {
+        padding: '$40 $50',
+        fontType: 'sm',
+      },
+      md: {
+        padding: '$40 $50',
+        fontType: 'base',
+      },
+      lg: {
+        padding: '$50 $60',
+        fontType: 'base',
+      }
+    }
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'md',
+  },
 });
