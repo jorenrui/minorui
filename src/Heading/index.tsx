9b94233114e9e42f3c98bb8459dbcd53e8c99b7e
Joeylene Rivera <joeylenerivera@gmail.com>
2021-02-27 20:49:41 +0800

    refactor: update heading sizes' font weights

--- src/Heading/index.tsx
+++ src/Heading/index.tsx
@@ -2,7 +2,6 @@ import { StitchesVariants } from '@stitches/core';
 import { styled } from '../lib/stitches';
 
 export const Heading = styled('h1', {
-  fontWeight: 500,
   letterSpacing: '-0.055em',
 
   variants: {
@@ -22,42 +21,55 @@ export const Heading = styled('h1', {
     size: {
       xs: {
         fontType: 'xs',
+        fontWeight: 600,
       },
       sm: {
         fontType: 'sm',
+        fontWeight: 600,
       },
       base: {
         fontType: 'base',
+        fontWeight: 600,
       },
       lg: {
         fontType: 'lg',
+        fontWeight: 600,
       },
       xl: {
         fontType: 'xl',
+        fontWeight: 600,
       },
       '2xl': {
         fontType: '2xl',
+        fontWeight: 600,
       },
       '3xl': {
         fontType: '3xl',
+        fontWeight: 500,
       },
       '4xl': {
         fontType: '4xl',
+        fontWeight: 500,
       },
       '5xl': {
         fontType: '5xl',
+        fontWeight: 500,
       },
       '6xl': {
         fontType: '6xl',
+        fontWeight: 500,
       },
       '7xl': {
         fontType: '7xl',
+        fontWeight: 500,
       },
       '8xl': {
         fontType: '8xl',
+        fontWeight: 500,
       },
       '9xl': {
         fontType: '9xl',
+        fontWeight: 500,
       },
     },
     color: {
