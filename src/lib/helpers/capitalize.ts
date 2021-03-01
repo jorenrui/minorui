002445a257affed3ade64df547ce8744698a3f56
Joeylene Rivera <joeylenerivera@gmail.com>
2021-03-01 14:41:04 +0800

    chore: add capitalize helper

---
+++ src/lib/helpers/capitalize.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns a capitalized string.
+ *
+ * @param string The string to be capitalize
+ */
+export const capitalize = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
