8334366e016d0d66b22c09f17b43eb18a28694e5
Joeylene Rivera <joeylenerivera@gmail.com>
2021-03-15 20:33:42 +0800

    feat: add display variant to Box

--- src/Box/index.tsx
+++ src/Box/index.tsx
@@ -1,4 +1,21 @@
 import { css, styled } from '../lib/stitches';
 
 export const box = css({});
-export const Box = styled('div', {});
+export const Box = styled('div', {
+  variants: {
+    display: {
+      block: {
+        display: 'block',
+      },
+      'inline-block': {
+        display: 'inline-block',
+      },
+      flex: {
+        display: 'flex',
+      },
+      grid: {
+        display: 'grid',
+      },
+    }
+  },
+});
