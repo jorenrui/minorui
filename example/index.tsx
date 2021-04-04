53cbe31b27ed0eab542ae11f33339a469da8ea33
Joeylene Rivera <joeylenerivera@gmail.com>
2021-04-04 22:58:45 +0800

    feat: add separator to example

--- example/index.tsx
+++ example/index.tsx
@@ -2,6 +2,7 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { Separator } from '../dist';
 import { Container } from './components/Container';
 import { Inputs } from './components/Inputs';
 import { Surfaces } from './components/Surfaces';
@@ -15,11 +16,17 @@ const App = () => {
   return (
     <Container>
       <Feedback />
+      <Separator />
       <Inputs />
+      <Separator />
       <DataDisplay />
+      <Separator />
       <Surfaces />
+      <Separator />
       <Navigation />
+      <Separator />
       <Typography />
+      <Separator />
       <Progress />
     </Container>
   );
--- src/Separator/index.tsx
+++ src/Separator/index.tsx
@@ -2,7 +2,7 @@ import * as RadixSeparator from '@radix-ui/react-separator';
 import { styled } from '../lib/stitches';
 
 export const Separator = styled(RadixSeparator.Root, {
-  backgroundColor: '$gray100',
+  backgroundColor: '$gray300',
   height: '$px',
   margin: '20px 0',
 });
